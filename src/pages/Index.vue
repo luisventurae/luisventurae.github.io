be0d8961cd2b12020395af51f98295bdb778c53e
Luis Ventura <luis.ventura@securitec.pe>
2021-05-29 17:15:47 -0500

    [inserted] meta tags open graph

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,21 +5,75 @@
 </template>
 
 <script>
-import HomeTemplate from "@/components/templates/HomeTemplate.vue"
+import HomeTemplate from "@/components/templates/HomeTemplate.vue";
 
 export default {
   metaInfo: {
     title: "Portafolio",
     meta: [
       {
-        key: 'fullstack, js, javascript, node, nodejs, ingeniero, sistemas, web, aplicacion, engineer, luisventurae, luis, miguel, ventura, universidad, autonoma, peru, leader, lima',
-      }
+        name: "robots",
+        content: "index, follow",
+      },
+      {
+        name: "googlebot",
+        content: "index, follow",
+      },
+      {
+        key:
+          "fullstack, js, javascript, node, nodejs, ingeniero, sistemas, web, aplicacion, engineer, luisventurae, luis, miguel, ventura, universidad, autonoma, peru, leader, lima",
+      },
+      {
+        property: "og:title",
+        content: "Luis Miguel Ventura | Portafolio",
+      },
+      {
+        property: "og:url",
+        content: "https://luisventurae.github.io/",
+      },
+      {
+        property: "og:image",
+        content:
+          "https://luisventurae.github.io/assets/images/img-20190809-wa0092-400x711.jpeg",
+      },
+      {
+        property: "og:determiner",
+        content: "briefcase",
+      },
+      {
+        property: "og:locale",
+        content: "es_PE",
+      },
+      {
+        property: "og:type",
+        content: "profile",
+      },
+      {
+        property: "og:profile:first_name",
+        content: "Luis Miguel",
+      },
+      {
+        property: "og:profile:last_name",
+        content: "Ventura",
+      },
+      {
+        property: "og:profile:username",
+        content: "luisventurae",
+      },
+      {
+        property: "og:profile:gender",
+        content: "male",
+      },
+      {
+        name: "google-site-verification",
+        content: process.env.GRIDSOME_GOOGLE_VERIFICATION,
+      },
     ],
   },
   components: {
     HomeTemplate,
   },
-}
+};
 </script>
 
 <style></style>

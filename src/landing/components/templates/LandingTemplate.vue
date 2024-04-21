0a67f5a6b00f37c27fd8cef9c93adcbc402db27b
Luis Miguel Ventura E <luisigsystem@gmail.com>
2024-04-20 19:39:13 -0500

    arch scss

---
+++ src/landing/components/templates/LandingTemplate.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "LandingTemplate",
+  // props: {},
+  setup(props) {
+    console.log("props", props)
+  },
+})
+</script>
+
+<template>
+  <div>Hola</div>
+</template>
+
+<style lang="scss" scoped></style>
